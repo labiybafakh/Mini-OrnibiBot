1230fd95433ea9a925653a1a5c526259ff318fff
labiybafakh <labiybafakh@gmail.com>
2024-07-06 23:23:56 +0900

    added interpolation pattern for sinewave

--- src/main.cpp
+++ src/main.cpp
@@ -4,81 +4,92 @@
 #include <unistd.h>
 #include <math.h>
 #include <string.h>
+#include <atomic>
 #include "ESP32Servo.h"
-#define LEFT 21
-#define RIGHT 22
+
+#define LEFT 6
+#define RIGHT 7
 
 Servo left_servo;
 Servo right_servo;
 
-float flapping_frequency;
-
+struct flapping{
+  uint8_t amplitude;
+  uint8_t offset;
+  uint8_t roll;
+  double freq;
+};
 
 TaskHandle_t Task1;
 TaskHandle_t Task2;
 
 int pos;
 
-uint32_t current_time, last_time, dif_time;
+std::atomic<std::int_fast8_t> wing_position;
+
+uint16_t min_pulse_width = 1000; // Minimum pulse width in microseconds
+uint16_t max_pulse_width = 2000; // Maximum pulse width in microseconds
+
+int min_angle = -60;       // Minimum angle in degrees
+int max_angle = 60;        // Maximum angle in degrees
+
+uint16_t degreeToPulse(int8_t degree) {
+
+    // Ensure the input degree is within the valid range
+    if (degree < min_angle) degree = min_angle;
+    if (degree > max_angle) degree = max_angle;
+    
+    // Perform linear mapping
+    uint16_t pulse = (uint16_t)((degree - min_angle) * (max_pulse_width - min_pulse_width) / (max_angle - min_angle) + min_pulse_width);
+    
+    return pulse;
+}
+
+flapping *flapping_param;
 
 void paramUpdate( void * pvParameters ){
   Serial.print("Task1 running on core ");
   Serial.println(xPortGetCoreID());
-
+  int time_ = 0;
   for(;;){
 
-        delay(1);
+      uint16_t periode_ = 1000 / flapping_param->freq;
+      wing_position = flapping_param->amplitude * sin((2 * M_PI * time_) / periode_) + flapping_param->offset;
+
+      if (time_ < periode_) {
+          time_++;
+      } else {
+          time_ = 0;
+      }
+        
+      delay(1);
     }
 }
 void motorUpdate( void * pvParameters ){
   Serial.print("Task2 running on core ");
   Serial.println(xPortGetCoreID());
   for(;;){
-  
-  uint16_t timing_signal = abs((int) (1000/flapping_frequency));
-  // Serial.print(timing_signal);
-  // Serial.print("\t");
-  current_time = millis();
+    
+    left_servo.writeMicroseconds(degreeToPulse(wing_position));
+    right_servo.writeMicroseconds(degreeToPulse(wing_position) * -1); // to adjust for a different angle between left and right
 
-  if((current_time - last_time) >= (timing_signal) ){
-    // A full period has passed, reset the timer
-    last_time = current_time;
-  }
+    Serial.println(degreeToPulse(wing_position));
 
-  // Create a square wave by alternating between two states
-  if ((current_time - last_time) < (int)(timing_signal*0.5)) {
-    // We are in the first half of the period
-    left_servo.write(1500 + (int)(50*7.778f));
-    right_servo.write(1500 - (int)(50*7.778f));
-    // Serial.println(1);
-  } 
-  else{
-    // We are in the second half of the period
-    left_servo.write(1500 + (int)(-10*7.778f));
-    right_servo.write(1500 - (int)(-10*7.778f));
-    // Serial.println(2);
-  }
-
-  // Serial.println((timing_signal*0.5));
-
-  delay(1);
+    delay(1);
 
   }
 }
 
 void setup() {
+  flapping_param = (flapping *) malloc(sizeof(flapping));
+
   Serial.begin(115200);
-  while(!Serial);
-	// ESP32PWM::allocateTimer(0);
-	// ESP32PWM::allocateTimer(1);
-	// ESP32PWM::allocateTimer(2);
-	// ESP32PWM::allocateTimer(3);
 
-  left_servo.setPeriodHertz(50);
-  left_servo.attach(LEFT, 800, 2200);
+  left_servo.setPeriodHertz(300);
+  left_servo.attach(LEFT, min_pulse_width, max_pulse_width);
 
-  right_servo.setPeriodHertz(50);
-  right_servo.attach(RIGHT, 800, 2200);
+  right_servo.setPeriodHertz(300);
+  right_servo.attach(RIGHT, min_pulse_width, max_pulse_width);
 
   xTaskCreatePinnedToCore(
                     paramUpdate,   /* Task function. */
@@ -101,9 +112,8 @@ void setup() {
 }
 
 void loop() {
-    flapping_frequency = 2;
+    flapping_param->amplitude = 30;
+    flapping_param->offset = 0;
+    flapping_param->freq = 4;
 }
 
-// void serialEvent(){
-//   if(Serial.available())
-// }
