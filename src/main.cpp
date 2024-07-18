e025753e39bccb296c7a6b1aacd772a3e2245c79
labiybafakh <labiybafakh@gmail.com>
2024-07-18 20:05:03 +0900

    added feature to receive UDP data

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,39 @@
 #include <string.h>
 #include <atomic>
 #include "ESP32Servo.h"
+#include <WiFi.h>
+#include <WiFiUdp.h>
 
-#define LEFT 6
-#define RIGHT 7
+const int servoPins[] = {D0, D1, D2, D3};  // Adjust these pins as needed
 
 Servo left_servo;
 Servo right_servo;
+Servo pitch_servo;
+Servo roll_servo;
+
+WiFiUDP udp;
+
+const char* ssid = "ornibibot";
+const char* password = "ornibibot5208";
+const int udpPort = 4210;
+
+// Define the static IP address for the AP
+IPAddress local_ip(192,168,3,15);
+IPAddress gateway(192,168,3,15);
+IPAddress subnet(255,255,255,0);
+
 
 struct flapping{
   uint8_t amplitude;
   uint8_t offset;
   uint8_t roll;
   double freq;
+};
+
+struct ornibibot_param{
+  std::atomic<float> frequency;
+  std::atomic<int8_t> roll;
+  std::atomic<int8_t> pitch;
 };
 
 TaskHandle_t Task1;
@@ -27,11 +48,13 @@ int pos;
 
 std::atomic<std::int_fast8_t> wing_position;
 
-uint16_t min_pulse_width = 1000; // Minimum pulse width in microseconds
-uint16_t max_pulse_width = 2000; // Maximum pulse width in microseconds
+uint16_t min_pulse_width = 920; // Minimum pulse width in microseconds
+uint16_t max_pulse_width = 2120; // Maximum pulse width in microseconds
 
 int min_angle = -60;       // Minimum angle in degrees
 int max_angle = 60;        // Maximum angle in degrees
+
+ornibibot_param ornibibot_parameter;
 
 uint16_t degreeToPulse(int8_t degree) {
 
@@ -45,15 +68,33 @@ uint16_t degreeToPulse(int8_t degree) {
     return pulse;
 }
 
+uint16_t degreeToPulseTail(int8_t degree) {
+    uint16_t min_pulse_width_ = 1100; // Minimum pulse width in microseconds
+    uint16_t max_pulse_width_ = 1900; // Maximum pulse width in microseconds
+
+    int min_angle_ = -45;       // Minimum angle in degrees
+    int max_angle_ = 45;        // Maximum angle in degrees
+
+    // Ensure the input degree is within the valid range
+    if (degree < min_angle_) degree = min_angle_;
+    if (degree > max_angle_) degree = max_angle_;
+    
+    // Perform linear mapping
+    uint16_t pulse = (uint16_t)((degree - min_angle_) * (max_pulse_width_ - min_pulse_width_) / (max_angle_ - min_angle_) + min_pulse_width_);
+    
+    return pulse;
+}
+
 flapping *flapping_param;
 
 void paramUpdate( void * pvParameters ){
   Serial.print("Task1 running on core ");
   Serial.println(xPortGetCoreID());
   int time_ = 0;
+  const TickType_t xDelay = 1 / portTICK_PERIOD_MS;
   for(;;){
 
-      uint16_t periode_ = 1000 / flapping_param->freq;
+      uint16_t periode_ = 1000 / ornibibot_parameter.frequency;
       wing_position = flapping_param->amplitude * sin((2 * M_PI * time_) / periode_) + flapping_param->offset;
 
       if (time_ < periode_) {
@@ -62,22 +103,45 @@ void paramUpdate( void * pvParameters ){
           time_ = 0;
       }
         
-      delay(1);
+      delay(xDelay);
     }
 }
 void motorUpdate( void * pvParameters ){
   Serial.print("Task2 running on core ");
   Serial.println(xPortGetCoreID());
+  const TickType_t xDelay = 5 / portTICK_PERIOD_MS;
   for(;;){
     
-    left_servo.writeMicroseconds(degreeToPulse(wing_position));
-    right_servo.writeMicroseconds(degreeToPulse(wing_position) * -1); // to adjust for a different angle between left and right
+    if(ornibibot_parameter.frequency < 0.5){
+        left_servo.writeMicroseconds(degreeToPulse(0));
+        right_servo.writeMicroseconds(degreeToPulse(-30)); // to adjust for a different angle between left and right
+        roll_servo.writeMicroseconds(degreeToPulseTail(0));
+        pitch_servo.writeMicroseconds(degreeToPulseTail(30*-1));
+    }
 
-    Serial.println(degreeToPulse(wing_position));
+    else{
+        left_servo.writeMicroseconds(degreeToPulse(wing_position));
+        right_servo.writeMicroseconds(degreeToPulse(-wing_position)); // to adjust for a different angle between left and right
+        roll_servo.writeMicroseconds(degreeToPulseTail(0));
+        pitch_servo.writeMicroseconds(degreeToPulseTail(30*-1));
+    }
 
-    delay(1);
-
+    vTaskDelay(xDelay);
   }
+}
+
+void deserializeUDP(){
+    uint8_t buffer[3] = {0, 0, 0};
+    int packetSize = udp.parsePacket();
+
+    if(packetSize>2){
+        udp.read(buffer, sizeof(buffer));
+
+        ornibibot_parameter.frequency = (float)buffer[0]*0.1f;
+        ornibibot_parameter.roll = (int8_t) buffer[1];
+        ornibibot_parameter.pitch = (int8_t) buffer[2];
+    }
+
 }
 
 void setup() {
@@ -86,10 +150,22 @@ void setup() {
   Serial.begin(115200);
 
   left_servo.setPeriodHertz(300);
-  left_servo.attach(LEFT, min_pulse_width, max_pulse_width);
+  left_servo.attach(servoPins[0], min_pulse_width, max_pulse_width);
 
   right_servo.setPeriodHertz(300);
-  right_servo.attach(RIGHT, min_pulse_width, max_pulse_width);
+  right_servo.attach(servoPins[1], min_pulse_width, max_pulse_width);
+
+  pitch_servo.setPeriodHertz(250);
+  pitch_servo.attach(servoPins[2], 1100, 1900);
+
+  roll_servo.setPeriodHertz(250);
+  roll_servo.attach(servoPins[3], 1100, 1900);
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+
+  udp.begin(udpPort);
 
   xTaskCreatePinnedToCore(
                     paramUpdate,   /* Task function. */
@@ -109,11 +185,15 @@ void setup() {
                     &Task2,      /* Task handle to keep track of created task */
                     1);          /* pin task to core 0 */
   delay(500);
+
+
 }
 
 void loop() {
-    flapping_param->amplitude = 30;
+    flapping_param->amplitude = 60;
     flapping_param->offset = 0;
-    flapping_param->freq = 4;
-}
+    deserializeUDP();
 
+    delay(5);
+
+}
