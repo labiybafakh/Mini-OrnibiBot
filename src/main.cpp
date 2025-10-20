ab5905d2284691fb2d91621dd3fc3162f043966b
labiybafakh <labiybafakh@gmail.com>
2025-10-20 21:45:37 +0900

    Adapt for the orange wing

--- src/main.cpp
+++ src/main.cpp
@@ -107,10 +107,10 @@ void paramUpdate( void * pvParameters ){
   for(;;){
 
       uint16_t periode_ = 1000 / ornibibot_parameter.frequency;
-      wing_position = (flapping_param->amplitude * sin((2 * M_PI * time_) / periode_)) + flapping_param->offset;
+      wing_position = (flapping_param->amplitude * sin((2 * M_PI * time_) / periode_)) ;
 
-      if(wing_position > 0) wing_position = flapping_param->amplitude;
-      else wing_position = flapping_param->amplitude * -1;
+      if(wing_position > 0) wing_position = flapping_param->amplitude + flapping_param->offset;
+      else wing_position = (flapping_param->amplitude + flapping_param->offset) * -1;
 
       if (time_ < periode_) {
           time_++;
@@ -128,7 +128,7 @@ void motorUpdate( void * pvParameters ){
   const TickType_t xDelay = 5 / portTICK_PERIOD_MS;
   for(;;){
 
-    const int adjustment = -10;
+    const int adjustment = 0;
     const int minimum_pitch_tail = 20;
     
     if(ornibibot_parameter.frequency < 0.5){
@@ -137,8 +137,8 @@ void motorUpdate( void * pvParameters ){
 
 
         setPosition(
-          degToSignal((25-ornibibot_parameter.roll+adjustment)),
-          degToSignal((25+ornibibot_parameter.roll)*-1),
+          degToSignal((25+ornibibot_parameter.roll-adjustment)),
+          degToSignal((25-ornibibot_parameter.roll)*-1),
           degToSignalTail(tail*-1),
           degToSignalTail(tail)
         );
@@ -158,8 +158,8 @@ void motorUpdate( void * pvParameters ){
         // }
         // else{
           setPosition(
-          degToSignal((wing_position-ornibibot_parameter.roll+adjustment)),
-          degToSignal((wing_position+ornibibot_parameter.roll)*-1),
+          degToSignal((wing_position+ornibibot_parameter.roll-adjustment)),
+          degToSignal((wing_position-ornibibot_parameter.roll)*-1),
           degToSignalTail(tail*-1),
           degToSignalTail(tail)
         );
